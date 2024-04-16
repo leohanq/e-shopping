38f9329dccce03997fd601ef08f4b1c51d02fd1f
leonardo.quintero <Leonardo.Hansel.H.Quintero.Contreras.-ND@disney.com>
2024-04-15 20:10:47 -0500

    cors security controller and docker compose front

--- product-microservice/src/main/java/com/eshopping/product/controller/ProductController.java
+++ product-microservice/src/main/java/com/eshopping/product/controller/ProductController.java
@@ -4,6 +4,7 @@ package com.eshopping.product.controller;
 import com.eshopping.product.controller.service.ProductService;
 import com.eshopping.product.model.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
+    @CrossOrigin("http://localhost:4200")
     public List<Product> getProducts() {
         return productService.allProduct();
         //User user = productService.getUserById(1);
